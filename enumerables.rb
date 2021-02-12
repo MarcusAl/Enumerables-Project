bb39e15ac74f5fe7a17f9f95b3bc84018c4fea1c
juxsalley <juxsalley@gmail.com>
2021-02-12 10:30:25 +0000

    add required fix

--- enumerables.rb
+++ enumerables.rb
@@ -1,103 +1,187 @@
-module Enumerable
-  def my_each(block = nil)
-    return to_enum(:self) unless block_given?
+module Enumerables
 
-    i = 0
-    while i < size
-     !block.nil? ? block.call(self[i]) : yield(self[i])
-     i += 1
+  def my_each(block = nil)
+    index = 0
+    if block_given?
+      size.times do
+        if !block.nil?
+          block.call(self[index])
+        else
+          yield(self[index])
+        end
+        index += 1
+      end
+    else
+      self
     end
-    self
   end
 
   def my_each_with_index(block = nil)
-    return to_enum(:my_each_with_index) unless block_given?
-
-    i = 0
-    while i < size
-      !block.nil? ? block.call(self[i], i) : yield(self[i], i)
-      i += 1
+    index = 0
+    if block_given?
+      size.times do
+        if !block.nil?
+          block.call(self[index], index)
+        else
+          yield(self[index], index)
+        end
+        index += 1
+      end
+    else
+      self
     end
-    self
   end
 
   def my_select(block = nil)
-    return to_enum(:my_each) unless block_given?
-
-    i = 0
-    self.times(self.length) do
-      if !block.nil?
-        puts self[i] if block.call(self[i])
-      elsif yield(self[i])
-        puts self[i]
+    index = 0
+    if block_given?
+      size.times do
+        if !block.nil?
+          puts self[index] if block.call(self[index])
+        elsif yield(self[index])
+          puts self[index]
+        end
+        index += 1
       end
-      i += 1
-    end
-  end
-
-  def my_all?(found = nil)
-    return to_enum(:my_all?) unless block_given?
-
-    my_each do |i|
-      return false if yield(i) == false or found.call(self[i])
-    end
-    true
-  end
-
-  def my_any?
-    return to_enum(:my_any?) unless block_given?
-
-    my_each do |i|
-      return true if yield(i) == true
-    end
-    false
-  end
-
-  def my_none?
-    return to_enum(:my_none?) unless block_given?
-
-    my_any? do |i|
-      return false if yield(i) == true
-    end
-    true
-  end
-
-  def my_count
-    return size unless block_given?
-
-    counter = 0
-    my_each do |i|
-      counter += 1 if yield(i) == true
-    end
-    counter
-  end
-
-  def my_map(&myproc)
-    return self unless block_given?
-
-    arr = []
-    my_each do |i|
-      arr << myproc.call(i)
-    end
-    arr
-  end
-
-  def my_inject(initial = 1, sym = nil)
-    if sym
-      my_each { |e| initial = initial.method(sym).call(e) }
     else
-      my_each { |e| initial = yield(initial, e) }
+      my_each
     end
-    initial
+  end
+
+  def my_all?(match = nil)
+    index = 0
+    statement = true
+    if block_given?
+      size.times do
+        statement = false unless yield(self[index])
+        index += 1
+      end
+    elsif !match.nil?
+      size.times do
+        begin
+          statement = false if self[index].is_a?(match)
+        rescue StandardError
+          statement = false if self[index].scan(match)
+        end
+      end
+    end
+    statement
+  end
+
+  def my_none?(match = nil)
+    index = 0
+    statement = false
+    statement = true if size.zero?
+    if block_given?
+      size.times do
+        statement = true unless yield(self[index])
+        index += 1
+      end
+    elsif !match.nil?
+      size.times do
+        begin
+          statement = true if self[i].is_a?(match)
+        rescue StandardError
+          statement = true if self[i].scan(match)
+        end
+        index += 1
+      end
+    else
+      size.times do
+        return true if self[index] == true or self[index].nil?
+
+        index += 1
+      end
+    end
+    statement
+  end
+
+  def my_any?(match = nil)
+    index = 0
+    statement = false
+    if block_given?
+      size.times do
+        return true if yield(self[index])
+
+        index += 1
+      end
+    elsif !match.nil?
+      size.times do
+        begin
+          statement = true if self[index].is_a?(match)
+        rescue StandardError
+          statement = true if self[index].scan(match)
+        end
+      end
+    else
+      size.times do
+        return true if self[index] != false or !self[index].nil?
+      end
+    end
+    statement
+  end
+
+  def my_count(match = nil)
+    index = 0
+    if block_given?
+      c = 0
+      size.times do
+        c += 1 if yield(self[index])
+        index += 1
+      end
+      c
+    elsif !match.nil?
+      c = 0
+      size.times do
+        c += 1 if self[index] == match
+        index += 1
+      end
+      c
+    else
+      size.times do
+        index += 1
+      end
+      index
+    end
+  end
+
+  def my_map(block = nil)
+    index = 0
+    new_array = []
+    my_array = []
+    my_array = if respond_to?(:to_ary)
+                 self
+               else
+                 to_a
+               end
+    if !block.nil?
+      my_array.size.times do
+        new_array.push(block.call(my_array[index]))
+        index += 1
+      end
+    else
+      my_array.size.times do
+        new_array.push(yield(my_array[index]))
+        index += 1
+      end
+    end
+    new_array
+  end
+
+  def my_inject(block = nil)
+    my_array = []
+    if !block.nil?
+      my_array.my_inject { |sum, x| sum + x }
+    elsif block_given?
+      accumulator = my_array[0]
+      my_array.my_each_with_index do |n, index|
+        accumulator = yield(accumulator, n) if index != 0
+      end
+      accumulator
+    end
   end
 end
 
-def multiply_els(arr)
-  arr.my_inject { |v, i| v * i }
+def multiply_els(array)
+  array.my_inject { |sum, n| sum * n }
 end
-
-array = [6, 19, 19, 25, 7, 30, 20, 27, 22, 19, 18, 29, 12, 31, 2, 12, 0, 32, 1, 20]
-block = proc { |num| num.even? }
-rang = Range.new(5, 50)
-
-puts rang.my_select(&block) == rang.select(&block)
