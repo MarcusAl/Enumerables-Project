f8f3927a0c3d06e8d944f1c9d884b9e47be79a43
juxsalley <juxsalley@gmail.com>
2021-02-13 20:28:18 +0000

    fix linters error

--- enumerables.rb
+++ enumerables.rb
@@ -1,3 +1,4 @@
+# rubocop:disable Style/CaseEquality
 module Enumerable
   def my_each
     return to_enum unless block_given?
@@ -8,7 +9,7 @@ module Enumerable
     self
   end
 
-# rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/CyclomaticComplexity
   def my_each_with_index
     return to_enum unless block_given?
 
@@ -38,7 +39,7 @@ module Enumerable
     count
   end
 
-# rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/CyclomaticComplexity
   def my_all?(param = nil)
     if block_given?
       to_a.my_each { |item| return false if yield(item) == false }
@@ -55,7 +56,7 @@ module Enumerable
     true
   end
 
-# rubocop:disable Metrics/CyclomaticComplexity
+  # rubocop:disable Metrics/CyclomaticComplexity
   def my_any?(input = nil)
     if block_given?
       to_a.my_each { |ele| return true if yield(ele) }
@@ -87,7 +88,8 @@ module Enumerable
     end
     arr
   end
-# rubocop:disable Metrics/CyclomaticComplexity
+
+  # rubocop:disable Metrics/CyclomaticComplexity
   def my_inject(start = nil, sym = nil)
     if (!start.nil? && sym.nil?) && (start.is_a?(Symbol) || start.is_a?(String))
       sym = start
