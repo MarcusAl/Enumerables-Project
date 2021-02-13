ff5ebca47bb96e81a89277ae7eea81dec5ca5765
juxsalley <juxsalley@gmail.com>
2021-02-13 12:37:23 +0000

    fix changes required by TSE

--- enumerables.rb
+++ enumerables.rb
@@ -1,186 +1,92 @@
 module Enumerable
-  def my_each(block = nil)
-    index = 0
+  def my_each
+    return to_enum unless block_given?
+
+    to_a.length.times do |index|
+      yield to_a[index]
+    end
+    self
+  end
+
+  def my_each_with_index
+    return to_enum unless block_given?
+
+    to_a.length.times do |index|
+      yield(to_a[index], index)
+    end
+    self
+  end
+
+  def my_select
+    return to_enum unless block_given?
+
+    results = []
+    my_each { |item| results.push(item) if yield item }
+    results
+  end
+
+  def my_all?(input = nil)
     if block_given?
-      size.times do
-        if !block.nil?
-          block.call(self[index])
-        else
-          yield(self[index])
-        end
-        index += 1
-      end
+      to_a.my_each { |fdm| return false if yield(fdm) == false }
+      return true
+    elsif input.nil?
+      to_a.my_each { |fdm| return false if fdm.nil? || fdm == false }
+    elsif !input.nil? && (input.is_a? Class)
+      to_a.my_each { |fdm| return false unless [fdm.class, fdm.class.superclass].include?(input) }
+    elsif !input.nil? && input.instance_of?(Regexp)
+      to_a.my_each { |fdm| return false unless fdm.match(fdm) }
     else
-      self
+      to_a.my_each { |fdm| return false if fdm != input }
     end
+    true
   end
 
-  def my_each_with_index(&block)
-    index = 0
+  def my_any?(input = nil)
     if block_given?
-      size.times do
-        if !block.nil?
-          block.call(self[index], index)
-        else
-          yield(self[index], index)
-        end
-        index += 1
-      end
+      to_a.my_each { |ele| return true if yield(ele) }
+      return false
+    elsif input.nil?
+      to_a.my_each { |ele| return true if ele }
+    elsif !input.nil? && (input.is_a? Class)
+      to_a.my_each { |ele| return true if [ele.class, ele.class.superclass].include?(input) }
+    elsif !input.nil? && input.instance_of?(Regexp)
+      to_a.my_each { |ele| return true if input.match(ele) }
     else
-      self
+      to_a.my_each { |ele| return true if ele == input }
     end
+    false
   end
 
-  def my_select(block = nil)
-    index = 0
-    if block_given?
-      size.times do
-        if !block.nil?
-          puts self[index] if block.call(self[index])
-        elsif yield(self[index])
-          puts self[index]
-        end
-        index += 1
-      end
+  def my_none?(input = nil, &block)
+    !my_any?(input, &block)
+  end
+
+  def my_map(proc = nil)
+    return to_enum unless block_given? or !proc.nil?
+
+    arr = []
+    if proc.nil?
+      to_a.my_each { |ele| arr << yield(ele) }
     else
-      my_each
+      to_a.my_each { |ele| arr << proc.call(ele) }
     end
+    arr
   end
 
-  def my_all?(match = nil)
-    index = 0
-    statement = true
-    if block_given?
-      size.times do
-        statement = false unless yield(self[index])
-        index += 1
-      end
-    elsif !match.nil?
-      size.times do
-        begin
-          statement = false if self[index].is_a?(match)
-        rescue StandardError
-          statement = false if self[index].scan(match)
-        end
-      end
+  def my_inject(start = nil, sym = nil)
+    if (!start.nil? && sym.nil?) && (start.is_a?(Symbol) || start.is_a?(String))
+      sym = start
+      start = nil
     end
-    statement
-  end
-
-  def my_none?(match = nil)
-    index = 0
-    statement = false
-    statement = true if size.zero?
-    if block_given?
-      size.times do
-        statement = true unless yield(self[index])
-        index += 1
-      end
-    elsif !match.nil?
-      size.times do
-        begin
-          statement = true if self[i].is_a?(match)
-        rescue StandardError
-          statement = true if self[i].scan(match)
-        end
-        index += 1
-      end
+    if !block_given? && !sym.nil?
+      to_a.my_each { |item| start = start.nil? ? item : start.send(sym, item) }
     else
-      size.times do
-        return true if (self[index] == true) || self[index].nil?
-
-        index += 1
-      end
+      to_a.my_each { |item| start = start.nil? ? item : yield(start, item) }
     end
-    statement
-  end
-
-  def my_any?(match = nil)
-    index = 0
-    statement = false
-    if block_given?
-      size.times do
-        return true if yield(self[index])
-
-        index += 1
-      end
-    elsif !match.nil?
-      size.times do
-        begin
-          statement = true if self[index].is_a?(match)
-        rescue StandardError
-          statement = true if self[index].scan(match)
-        end
-      end
-    else
-      size.times do
-        return true if (self[index] != false) || !self[index].nil?
-      end
-    end
-    statement
-  end
-
-  def my_count(match = nil)
-    index = 0
-    if block_given?
-      c = 0
-      size.times do
-        c += 1 if yield(self[index])
-        index += 1
-      end
-      c
-    elsif !match.nil?
-      c = 0
-      size.times do
-        c += 1 if self[index] == match
-        index += 1
-      end
-      c
-    else
-      size.times do
-        index += 1
-      end
-      index
-    end
-  end
-
-  def my_map(block = nil)
-    index = 0
-    new_array = []
-    my_array = if respond_to?(:to_ary)
-                 self
-               else
-                 to_a
-               end
-
-    if !block.nil?
-      my_array.size.times do
-        new_array.push(block.call(my_array[index]))
-        index += 1
-      end
-    else
-      my_array.size.times do
-        new_array.push(yield(my_array[index]))
-        index += 1
-      end
-    end
-    new_array
-  end
-
-  def my_inject(block = nil)
-    my_array = []
-    if !block.nil?
-      my_array.my_inject { |sum, x| sum + x }
-    elsif block_given?
-      accumulator = my_array[0]
-      my_array.my_each_with_index do |n, index|
-        accumulator = yield(accumulator, n) if index != 0
-      end
-      accumulator
-    end
+    start
   end
 end
 
-def multiply_els(arr)
-  arr.my_inject { |sum, n| sum * n }
+def multiply_els(input)
+  input.my_inject(:*)
 end
